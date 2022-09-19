0ef0d7f0343f3fcf250adeaa3b49ab4f41bcf06f
Nicolas-Juliani <112420529+Nicolas-Juliani@users.noreply.github.com>
2022-09-19 10:13:43 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -4,7 +4,7 @@
 import streamlit as st
   
 # Use st.title("") para adicionar um TÍTULO ao seu Web app
-st.title("Meu prof. é Incrível!")
+st.title("Meu prof. Gil Cebola é Incrível!")
 
 # Use st.header("") para adicionar um CABEÇALHO ao seu Web app
 st.header("Cabeçalho")
